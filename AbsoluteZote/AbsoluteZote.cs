bdbf8436b4a57c6b59d0c88f2ba4815f9c03ea28
Njord Yu <yunjord@gmail.com>
2022-04-13 23:21:04 -0700

    Updated.

---
+++ AbsoluteZote/AbsoluteZote.cs
@@ -0,0 +1,16 @@
+﻿using Modding;
+using System.Collections.Generic;
+using UnityEngine;
+using Satchel;
+
+
+namespace AbsoluteZote
+{
+    public class AbsoluteZote : Mod
+    {
+        public AbsoluteZote() : base("AbsoluteZote")
+        {
+        }
+        public override string GetVersion() => "1.0";
+    }
+}
