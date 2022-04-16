609d68913b17c3052b4674336854dec694ac596c
Njord Yu <yunjord@gmail.com>
2022-04-16 01:45:32 -0700

    Updated.

--- AbsoluteZote/AbsoluteZote.cs
+++ AbsoluteZote/AbsoluteZote.cs
@@ -12,5 +12,17 @@ namespace AbsoluteZote
         {
         }
         public override string GetVersion() => "1.0";
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            On.PlayMakerFSM.OnEnable += PlayMakerFSMOnEnable;
+        }
+        private void PlayMakerFSMOnEnable(On.PlayMakerFSM.orig_OnEnable original, PlayMakerFSM fsm)
+        {
+            if (fsm.gameObject.scene.name == "GG_Radiance" && fsm.gameObject.name == "Boss Control" && fsm.FsmName == "Control")
+            {
+                this.LogFSM(fsm);
+            }
+            original(fsm);
+        }
     }
 }
