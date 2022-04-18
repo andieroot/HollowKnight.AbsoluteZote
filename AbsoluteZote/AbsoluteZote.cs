837a4dda2e040d4ba59c98f0ca35efc6a12431ef
Njord Yu <yunjord@gmail.com>
2022-04-17 22:21:43 -0700

    Updated.

--- AbsoluteZote/AbsoluteZote.cs
+++ AbsoluteZote/AbsoluteZote.cs
@@ -4,7 +4,7 @@ using Modding;
 
 namespace AbsoluteZote
 {
-    public class AbsoluteZote : Mod
+    public class AbsoluteZote : Mod, ITogglableMod
     {
         private Boss boss;
         public Title title;
@@ -33,8 +33,16 @@ namespace AbsoluteZote
             ModHooks.LanguageGetHook += LanguageGetHook;
             ModHooks.HeroUpdateHook += HeroUpdateHook;
             UnityEngine.SceneManagement.SceneManager.activeSceneChanged += ActiveSceneChanged;
-            foreach (var module in modules)
-                module.LoadPrefabs(preloadedObjects);
+            if(preloadedObjects != null)
+                foreach (var module in modules)
+                    module.LoadPrefabs(preloadedObjects);
+        }
+        public void Unload()
+        {
+            On.PlayMakerFSM.OnEnable -= PlayMakerFSMOnEnable;
+            ModHooks.LanguageGetHook -= LanguageGetHook;
+            ModHooks.HeroUpdateHook -= HeroUpdateHook;
+            UnityEngine.SceneManagement.SceneManager.activeSceneChanged -= ActiveSceneChanged;
         }
         private void PlayMakerFSMOnEnable(On.PlayMakerFSM.orig_OnEnable original, PlayMakerFSM fsm)
         {
